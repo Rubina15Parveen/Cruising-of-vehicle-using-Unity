7902f3844be8b6feda2162447c243a9755e753c5
Rubina15Parveen <127428095+Rubina15Parveen@users.noreply.github.com>
2023-08-16 17:20:07 -0500

    Committing the full project here

---
+++ RTS Project 1/Problem1/Debug_Task.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debug_Task : TaskInterface
+{
+    public void Execute(DeviceRegistry devices) {
+        if (Input.GetKey("w")) {
+           /** Problem 1: AVL Environment */
+            devices.speedControl[0] = 1f;
+            devices.speedControl[1] = 10f;
+        }
+
+        if (Input.GetKey("s")) {
+            devices.speedControl[0] = 1f;
+            devices.speedControl[1] = -10f;
+        }
+
+        if (Input.GetKey("x")) {
+            devices.speedControl[0] = 1f;
+            devices.speedControl[1] = 0f;
+        }
+
+        if (Input.GetKey("a") || Input.GetKey("d")) {
+            devices.steeringControl[0] = 1f;
+
+            if (Input.GetKey("a")) {
+                devices.steeringControl[1] = -3;  //left
+            } else {
+                devices.steeringControl[1] = 3;   //right
+            }
+        } else {
+            devices.steeringControl[0] = 1f;
+            devices.steeringControl[1] = 0f;
+        }
+
+        if (Input.GetKey("space")) {
+            devices.brakeControl[0] = 1f;
+            devices.brakeControl[1] = Time.fixedDeltaTime;
+        }
+    }
+}
---
+++ avl-dev/Unity-AVL/Assets/Scripts/Tasks/TaskList.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskList : MonoBehaviour
+{
+    protected TaskInterface[] tasks = new TaskInterface[] {
+        // STUDENTS
+        // Instantiate your tasks here as decsribed in the documentation
+    };
+
+    public TaskInterface[] GetTasks() {
+        return this.tasks;
+    }
+}
